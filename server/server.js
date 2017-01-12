e33ac1e1ec9bcb5f633066ed348546676918d5ed
zerofruit <14wnrkim@gmail.com>
2017-01-12 16:31:20 +0000

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectId} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 const {mongoose}  = require('./db/mongoose');
 const {Todo}      = require('./models/todo');
@@ -43,15 +43,30 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if (!ObjectId.isValid(id)) {
-    return res.status(404).send({});
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(404).send({});
+      return res.status(404).send();
     }
     res.send({todo});
-  }).catch((e) => res.status(400).send({}));
+  }).catch((e) => res.status(400).send());
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if (!doc) {
+      res.status(404).send();
+    }
+    res.send({doc});
+  }).catch((e) => res.status(400).send())
 });
 
 app.listen(port, () => {
